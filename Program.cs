e509f1d067a2c88a93fec5b55dc58f1b8439a990
Slxdy <miketherealnerd@gmail.com>
2023-05-22 00:53:36 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.WebSocket;
+using EasyNetLog;
+using Newtonsoft.Json.Linq;
+
+namespace KostyasLairBot;
+
+internal class Program
+{
+    public static EasyNetLogger Logger { get; private set; } = new(msg => $"[<color=red>{DateTime.Now:HH:mm:ss.fff}</color>] {msg}", true, new string[] { Path.Combine(Directory.GetCurrentDirectory(), "latest.log") });
+
+    public static DiscordSocketClient Discord { get; private set; } = new();
+
+    private const string configPath = "config.json";
+
+    private static async Task Main()
+    {
+        if (!File.Exists(configPath))
+        {
+            Logger.Log($"Please set up a {configPath} file first.");
+            return;
+        }
+
+        var config = JObject.Parse(await File.ReadAllTextAsync(configPath));
+
+        var token = (string?)config["token"];
+        if (token == null)
+        {
+            Logger.Log($"Please set the token in the config file.");
+            return;
+        }
+
+        Discord.Log += OnDiscordLog;
+
+        await Discord.LoginAsync(TokenType.Bot, token);
+        await Discord.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private static Task OnDiscordLog(LogMessage message)
+    {
+        Logger.Log($"[<color=blue>Discord</color>] {message.Message}");
+        return Task.CompletedTask;
+    }
+}
