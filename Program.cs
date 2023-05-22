896195842dbe356d436b0454672c7990195ed8a2
Slxdy <michal2006.2006@gmail.com>
2023-05-22 15:38:31 +0200

    Added auto-updates from the production branch (this is unsafe af)

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using Discord.WebSocket;
 using EasyNetLog;
 using Newtonsoft.Json.Linq;
+using Octokit;
+using System.Diagnostics;
 
 namespace KostyasLairBot;
 
@@ -11,10 +13,16 @@ internal class Program
 
     public static DiscordSocketClient Discord { get; private set; } = new();
 
+    public static GitHubClient GitHub { get; private set; } = new(new ProductHeaderValue("KostyasLairBot"));
+
     private const string configPath = "config.json";
+    private const string commitShaPath = "commit.txt";
 
     private static async Task Main()
     {
+        Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+        Logger.Log($"Current dir set to {Directory.GetCurrentDirectory()}");
+
         if (!File.Exists(configPath))
         {
             Logger.Log($"Please set up a {configPath} file first.");
@@ -35,7 +43,37 @@ internal class Program
         await Discord.LoginAsync(TokenType.Bot, token);
         await Discord.StartAsync();
 
-        await Task.Delay(-1);
+        await GitCheckLoopAsync();
+
+        Logger.Log("<color=red>Exiting...</color>");
+    }
+
+    private static async Task GitCheckLoopAsync()
+    {
+        Directory.SetCurrentDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".."));
+        Logger.Log($"Current dir set to {Directory.GetCurrentDirectory()}");
+
+        await Process.Start("git", "checkout production").WaitForExitAsync();
+
+        for (; ; )
+        {
+            var branch = await GitHub.Git.Reference.Get("CTFAK", "KostyasLairBot", "production");
+            if (!File.Exists(commitShaPath) || await File.ReadAllTextAsync(commitShaPath) != branch.Object.Sha)
+            {
+                Logger.Log($"New update detected ({branch.Object.Sha}). Restarting...");
+
+                await File.WriteAllTextAsync(commitShaPath, branch.Object.Sha);
+
+                await Process.Start("git", "fetch").WaitForExitAsync();
+                await Process.Start("git", "pull origin production").WaitForExitAsync();
+
+                Process.Start("dotnet", "run");
+
+                return;
+            }
+
+            await Task.Delay(10000);
+        }
     }
 
     private static Task OnDiscordLog(LogMessage message)
