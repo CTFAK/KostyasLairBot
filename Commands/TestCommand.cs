10129427ce204c8917ebda3cbdf69b6c51562b5c
Slxdy <michal2006.2006@gmail.com>
2023-05-22 20:06:48 +0200

    Added support for commands

---
+++ Commands/TestCommand.cs
@@ -0,0 +1,15 @@
+﻿using Discord.WebSocket;
+
+namespace KostyasLairBot.Commands;
+
+internal class TestCommand : DiscordCommand
+{
+    public override string Name => "test";
+
+    public override string Description => "Just a test";
+
+    public override async Task OnExecute(SocketSlashCommand interaction)
+    {
+        await interaction.RespondAsync("This is indeed a test.");
+    }
+}
